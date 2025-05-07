8fbef15dc772eb1602b67fdb853f4b614e22d8ba
rschlager <Reinhold.Schlager@vector.com>
2025-05-07 14:12:34 +0200

    removed unused code.

--- text_analysis.py
+++ text_analysis.py
@@ -78,26 +78,6 @@ misspelled_words = spell.unknown([token.text for token in doc if token.is_alpha]
 
 print("Falsch geschriebene Wörter:", misspelled_words)
 
-#import spacy
-#import language_tool_python
-
-# Lade spaCy Modell
-#nlp = spacy.load('de_core_news_sm')
-
-# Beispieltext
-#text = "Dies ist ein Beispeiltext mit ein paar Fehlen."
-
-# LanguageTool für die Rechtschreib- und Grammatikprüfung
-#tool = language_tool_python.LanguageTool('de-DE') #zertifikat verify='C:/Users/vector_user/Vector_Root_CA_2.0.crt'
-
-# Überprüfe den Text auf Fehler
-#matches = tool.check(text)
-
-# Ausgabe der gefundenen Fehler
-#for match in matches:
-#    print(f"Fehler: {match.message}")
-#    print(f"Stellen: {match.context}")
-
 
 
 
