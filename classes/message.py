4c9cb5952df6a1ffcae4263adda0f79ce14eac3f
rschlager <Reinhold.Schlager@vector.com>
2025-05-23 16:45:26 +0200

    Added message class.

---
+++ classes/message.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+class Message:
+    
+    #TODO: sender hier notwendig? -> Nur wenn nach einzelnen Nachrichten gesucht werden muss. 
+
+    def __init__(self, chat_id, message_id, sender, timestamp, content, message_type, quoted_message):
+        self.chat_id = chat_id
+        self.message_id = message_id
+        self.sender = sender
+        self.timestamp = timestamp
+        self.content = content
+        self.message_type = message_type
+        self.quoted_message = quoted_message
+
+    def __str__(self):
+        toString = f"""ChatId: {self.chat_id}, MessageId: {str(self.message_id)}
+        Sender: {self.sender}
+        Timestamp: {self.timestamp}
+        Content: {self.content}
+        MessageType: {self.message_type}
+        quotedMessage: {{ {self.quoted_message } }}
+        """
+        return toString
+    
+    def hasQuote(self):
+        if self.quoted_message == None:
+            return True
+        return False
+
+if __name__ == "__main__":
+    msg_without_quote = Message(6, 1, 'Alice',  datetime.now(),"Hello what up", "Text", None)
+    print(msg_without_quote)
+    quote = Message(7, 1, 'Ute', datetime.now(), "Do you want to go outside?", "Text", None)
+    msg_with_quote = Message(7, 2, 'Bob', datetime.now(), "This sounds awesome!", "Text", quote)
+    print(msg_with_quote)
+
+    print(f"Has msg1 a quote? -> {msg_without_quote.hasQuote()}")
+    print(f"Has msg2 a quote? -> {msg_with_quote.hasQuote()}")
+    
+
