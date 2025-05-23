e20cdd8cb67e54b87498695ff878156576cf6235
rschlager <Reinhold.Schlager@vector.com>
2025-05-23 17:19:38 +0200

    Added basic author information.

--- classes/author.py
+++ classes/author.py
@@ -1,3 +1,23 @@
 class Author:
-    def __init__(self, name):
+    def __init__(self, author_id, name, last_name, age, gender, first_language, languages, region, job):
+        self.author_id = author_id
         self.name = name
+        self.last_name = last_name
+        self.age = age
+        self.gender = gender
+        self.first_language = first_language
+        self.languages = languages
+        self.region = region
+        self.job = job
+
+    def __str__(self):
+        return (f"Author({self.author_id}): {self.name} {self.last_name}, "
+                f"Age: {self.age}, Gender: {self.gender}, "
+                f"First Language: {self.first_language}, "
+                f"Other Languages: {', '.join(self.languages)}, "
+                f"Region: {self.region}, Job: {self.job}")
+
+
+if __name__ == "__main__":
+        author = Author(1, "Reinhold", "Schlager", 30, "Male", "Deutsch", ["Englisch", "Russisch"], "Bayern", "Softwareentwickler")
+        print(author)
--- classes/chat.py
+++ classes/chat.py
@@ -17,9 +17,16 @@ class Chat:
         for msg in self.messages:
             print(msg)
 
-    if __name__ == "__main__":
-        msg1 = Message(6, 1, 'Alice',  datetime.now(),"Hello what up", "Text")
-        msg2 = Message(7, 1, 'Ute', datetime.now(), "Do you want to go outside?", "Text")
-        msg3 = Message(7, 2, 'Bob', datetime.now(), "This sounds awesome!", "Text")
+if __name__ == "__main__":
+    chat = Chat(6)
 
+    msg1 = Message(6, 1, 'Alice',  datetime.now(),"Hello what up", "Text")
+    msg2 = Message(6, 2, 'Alice', datetime.now(), "Do you want to go outside?", "Text")
+    msg3 = Message(6, 3, 'Bob', datetime.now(), "This sounds awesome!", "Text")
+
+    chat.add_message(msg1)
+    chat.add_message(msg2)
+    chat.add_message(msg3)
+
+    chat.show_messages()
     
