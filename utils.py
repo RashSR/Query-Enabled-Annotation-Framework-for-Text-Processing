0d89eb372f1b11edcf69882a631adf0dfb8c7bca
rschlager <Reinhold.Schlager@vector.com>
2025-05-26 11:09:49 +0200

    added language tool anlyze to utils.

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import spacy
+import language_tool_python
 
-# Load german spaCy model
+# Load german spaCy model and initialize language tool
 nlp = spacy.load("de_core_news_lg") #possible values: de_core_news_sm de_core_news_md, de_core_news_lg (more powerful)
+tool = language_tool_python.LanguageTool('de-DE', remote_server='http://localhost:8081')
 
 def extract_text_from_html(filePath, withTags=False):
     from bs4 import BeautifulSoup
@@ -19,11 +21,6 @@ def extract_text_from_html(filePath, withTags=False):
     # Get only the inner HTML (without <body> tags)
     inner_html = body.decode_contents()
     return inner_html
-
-#useful to print spacy like annotations
-def print_tokennized_word(key, value):
-    if (len(value) != 0):
-        print(f"{key}: {value}")
 
 def analyze_msg_with_spacy(text):
     doc = nlp(text)
@@ -51,3 +48,70 @@ def analyze_msg_with_spacy(text):
         print_tokennized_word("Modus", token.morph.get('Mood')) #Indikativ/Konjunktiv
 
         print("---")
+
+#useful to print spacy like annotations
+def print_tokennized_word(key, value):
+    if (len(value) != 0):
+        print(f"{key}: {value}")
+
+def anaylze_msg_with_language_tool(text):
+    #check the text
+    matches = tool.check(text)
+
+    #convert text to a list to change it easily
+    text_list = list(text)
+
+    #find all different ruleIds
+    found_ruleIds = []
+
+    #print all found errors
+    for match in reversed(matches):
+        startPos = match.offset
+        endPos = match.offset + match.errorLength
+        fehlertext = text[startPos : endPos]
+        print_error(match, startPos, endPos, text)
+        add_error_tags(match.ruleId, fehlertext, startPos, endPos, text_list)
+        if not (found_ruleIds.__contains__((match.ruleId, match.message))):
+            found_ruleIds.append((match.ruleId, match.message))
+        print("---")
+
+    # Den bearbeiteten Text ausgeben
+    modified_text = ''.join(text_list)
+
+# print all usefull error informations
+def print_error(match, startPos, endPos, text):
+    fehlertext = text[startPos : endPos]
+    print(f"Gefundener Fehler: '{fehlertext}'")
+    print(f"Nachricht: {match.message}")
+    if(len(match.replacements) > 0):
+        print(f"Vorschlag: {match.replacements}")
+    print(f"Position: {startPos}-{endPos}")
+    print(convert_message(match.ruleId))
+    print(f"Fehlerregel ID: {match.ruleId}")
+    print(f"Kategorie: {match.category}")
+    print(f"RuleIssueType: {match.ruleIssueType}")
+
+#convert the match.messages in a useful description
+def convert_message(lt_message):
+    match lt_message:
+        case "GERMAN_SPELLER_RULE":
+            return "Tippfehler"
+        case "PLURAL_APOSTROPH":
+            return "falsches Apostroph bei Pluralbildung"
+        case "KOMMA_INFINITIVGRUPPEN":
+            return "Komma bei Infinitvgruppe"
+        case "DE_CASE":
+            return "Falsche Großschreibung"
+        case "GERMAN_WORD_REPEAT_RULE":
+            return "Wortwiederholung"
+        case "DE_DOUBLE_PUNCTUATION":
+            return "Doppelte Punktuation"
+        case "UPPERCASE_SENTENCE_START":
+            return "Großschreibung am Satzanfang"   
+        case _:
+            print("bisher unbekannt")
+
+#add html style tags
+def add_error_tags(ruleId, fehlertext, startPos, endPos, text_list):
+    error_tag = f"<span data-error=\"{ruleId}\">{fehlertext}</span>"
+    text_list[startPos : endPos] = list(error_tag)
