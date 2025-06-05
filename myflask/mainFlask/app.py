9e9cb9e8309f1872415d62762e989a1d4eef2bef
rschlager <Reinhold.Schlager@vector.com>
2025-06-05 11:39:59 +0200

    Added a profile card.

--- myflask/mainFlask/app.py
+++ myflask/mainFlask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime, timedelta
+from classes.author import Author
 import utils
 import locale
 locale.setlocale(locale.LC_TIME, 'German_Germany.1252') #This is for windows only -> mac/linux: locale.setlocale(locale.LC_TIME, 'de_DE.UTF-8')
@@ -8,10 +9,14 @@ app = Flask(__name__)
 app.jinja_env.globals.update(now=datetime.now, timedelta=timedelta)
 
 chats = utils.load_all_chats_from_files([1, 2])
+author = Author(0, "Reinhold", 30, "Male", "Deutsch", ["English", "Russisch"], "Bayern", "Softwareentwickler")
+author.add_chat(chats[0])
+author.add_chat(chats[1])
 
-@app.route("/")
-def index():
-    return render_template("chat.html", chats=chats, chat=chats[0], current_user="Reinhold")
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", author=author)
+
 
 @app.route("/chat/<int:chat_id>")
 def chat_view(chat_id):
