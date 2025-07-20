e3f33983f7cbc33df753f7ce5c8c3e31f2d76eb7
scr41255 <reinhold.schlager@st.oth-regensburg.de>
2025-07-20 17:55:05 +0200

    Added case distinction if value is string.

--- mainFlask/data/db_handling.py
+++ mainFlask/data/db_handling.py
@@ -329,8 +329,11 @@ def create_spacy_match(db: SQLAlchemy, app: Flask, spacy_match: SpacyMatch):
 
 def update_author(db: SQLAlchemy, app: Flask, author_id: int, column_name: str, value) -> bool:
     with app.app_context():
-        #TODO check if string
-        sql_text = f"UPDATE author SET {column_name} = '{value}' WHERE id = {author_id}"
+        
+        if isinstance(value, str):
+            value = f"'{value}'"
+        
+        sql_text = f"UPDATE author SET {column_name} = {value} WHERE id = {author_id}"
         result = db.session.execute(text(sql_text))
         if result.rowcount > 0:
             db.session.commit()
