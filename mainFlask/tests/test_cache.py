b27a992de4580c702954a55e85b844bf73aaee4c
rschlager <Reinhold.Schlager@vector.com>
2025-09-03 12:00:10 +0200

    added pytest fixture.

--- mainFlask/tests/test_cache.py
+++ mainFlask/tests/test_cache.py
@@ -1,5 +1,17 @@
+import pytest
 from mainFlask.classes.annotation import Annotation
 from mainFlask.data.cachestore import CacheStore
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+@pytest.fixture
+def establish_db_connection():
+    app = Flask(__name__)
+    app.secret_key = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/rschlager/Desktop/ElasticSearchTest/instance/mydatabase.db'  #TODO: this needs own db
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(app)
+    CacheStore.Instance(db, app)
 
 def test_CRUD_annotation():
     annotation = Annotation(None, 1, 3, 6, "RULE 43", "Das ist der Grund", "Mein Kommentar.")
@@ -18,7 +30,8 @@ def test_CRUD_annotation():
     if(CacheStore.Instance().delete_annotation_by_id(updated_annotation.id)):
         print("Erfolgreich gelöscht!")
 
-def test_get_all_annotations_by_msg_id():
+def test_get_all_annotations_by_msg_id(establish_db_connection):
+
     #arrange
     msg_id: int = 1
     annotation1 = Annotation(None, msg_id, 3, 6, "RULE 43", "Das ist der Grund", "Mein Kommentar.")
@@ -34,8 +47,13 @@ def test_get_all_annotations_by_msg_id():
     
     #assert
     assert len(annotations) == 2
-    assert all(a.msg_id == msg_id for a in annotations)
+    assert all(a.message_id == msg_id for a in annotations)
     assert {a.reason for a in annotations} == {"Das ist der Grund", "Ein anderer Grund"}
     assert created_annotation1 in annotations
     assert created_annotation2 in annotations 
     assert created_annotation3 not in annotations
+
+    CacheStore.Instance().delete_annotation_by_id(created_annotation1.id)
+    CacheStore.Instance().delete_annotation_by_id(created_annotation2.id)
+    CacheStore.Instance().delete_annotation_by_id(created_annotation3.id)
+
