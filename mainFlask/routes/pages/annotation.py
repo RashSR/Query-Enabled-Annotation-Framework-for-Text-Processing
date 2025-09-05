46e99d6adcd33a1072b3b7666e6708ec9a610297
rschlager <Reinhold.Schlager@vector.com>
2025-09-05 11:32:12 +0200

    added method to save comment.

--- mainFlask/routes/pages/annotation.py
+++ mainFlask/routes/pages/annotation.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 from mainFlask.data.cachestore import CacheStore
 from mainFlask.classes.message import Message
 from mainFlask.classes.annotation import Annotation
@@ -18,3 +18,17 @@ def annotation_view_message(message_id: int):
         #print(token)
     print(message.annotated_text)
     return render_template('annotation.html', message=message)
+
+def save_annotation_comment(annotation_id: int, new_comment: str):
+    annotation_to_update = CacheStore.Instance().get_annotation_by_id(annotation_id)
+    annotation_to_update.comment = new_comment
+    isUpdated = CacheStore.Instance().update_annotation(annotation_to_update)
+    return isUpdated
+
+@annotation_bp.route('/save_annotation_comment', methods=['POST'])
+def save_annotation_comment_route():
+    annotation_id = request.form.get('annotation_id', type=int)
+    new_comment = request.form.get('new_comment', type=str)
+    # Call your method (should return True/False)
+    success = save_annotation_comment(annotation_id, new_comment)
+    return jsonify({'success': bool(success)})
