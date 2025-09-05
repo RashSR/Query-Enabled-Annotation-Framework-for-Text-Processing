6516ac5cc7925007223b965037ead614726de77b
rschlager <Reinhold.Schlager@vector.com>
2025-09-05 11:50:59 +0200

    renamed update annotation route.

--- mainFlask/routes/pages/annotation.py
+++ mainFlask/routes/pages/annotation.py
@@ -20,9 +20,8 @@ def annotation_view_message(message_id: int):
     return render_template('annotation.html', message=message)
 
 
-def save_annotation_comment(annotation_id: int, new_comment: str, grund: str, annotation_text: str, start_pos: int, end_pos: int):
+def update_annotation(annotation_id: int, new_comment: str, grund: str, annotation_text: str, start_pos: int, end_pos: int):
     annotation_to_update = CacheStore.Instance().get_annotation_by_id(annotation_id)
-    print(f"{new_comment},{grund},{annotation_text},{start_pos},{end_pos}")
     annotation_to_update.comment = new_comment
     annotation_to_update.reason = grund
     annotation_to_update.annotation = annotation_text
@@ -31,14 +30,25 @@ def save_annotation_comment(annotation_id: int, new_comment: str, grund: str, an
     isUpdated = CacheStore.Instance().update_annotation(annotation_to_update)
     return isUpdated
 
-@annotation_bp.route('/save_annotation_comment', methods=['POST'])
+@annotation_bp.route('/update_annotation', methods=['POST'])
 
-def save_annotation_comment_route():
+def save_annotatio_route():
     annotation_id = request.form.get('annotation_id', type=int)
     new_comment = request.form.get('new_comment', type=str)
     grund = request.form.get('grund', type=str)
     annotation_text = request.form.get('annotation', type=str)
     start_pos = request.form.get('start_pos', type=int)
     end_pos = request.form.get('end_pos', type=int)
-    success = save_annotation_comment(annotation_id, new_comment, grund, annotation_text, start_pos, end_pos)
+    success = update_annotation(annotation_id, new_comment, grund, annotation_text, start_pos, end_pos)
+    return jsonify({'success': bool(success)})
+
+
+def delete_annotation(annotation_id: int):
+    print(f"try to delete {annotation_id}")
+    return CacheStore.Instance().delete_annotation_by_id(annotation_id)
+
+@annotation_bp.route('/delete_annotation', methods=['POST'])
+def delete_annotation_route():
+    annotation_id = request.form.get('annotation_id', type=int)
+    success = delete_annotation(annotation_id)
     return jsonify({'success': bool(success)})
