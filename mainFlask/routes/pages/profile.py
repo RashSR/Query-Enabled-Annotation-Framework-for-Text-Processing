566899a44e3bc20c07deaca97f268b70b763b749
rschlager <Reinhold.Schlager@vector.com>
2025-09-06 18:11:25 +0200

    connected modal to db.

--- mainFlask/routes/pages/profile.py
+++ mainFlask/routes/pages/profile.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, session, abort, request, jsonify
 import utils
 from mainFlask.data.cachestore import CacheStore
+from mainFlask.classes.author import Author
 
 profile_bp = Blueprint('profile', __name__)
 
@@ -30,5 +31,24 @@ def save_author_annotation(author_id):
         return jsonify({'error': 'Not found'}), 404
     author.annotation = annotation
     author = CacheStore.Instance().update_author(author, column_name="annotation", value=annotation)
+    return jsonify({'success': True})
+
+def create_author(name, age, gender, first_language, languages, region, job) -> bool:
+    author_to_create = Author(None, name, age, gender, first_language, languages, region, job)
+    created_author = CacheStore.Instance().create_author(author_to_create)
+    IsCreationSucessfully = created_author is not None
+    return IsCreationSucessfully
+
+@profile_bp.route('/add_author', methods=['POST'])
+def add_author():
+    data = request.get_json()
+    name = data.get('name', '')
+    age = data.get('age', '')
+    gender = data.get('gender', '')
+    first_language = data.get('first_language', '')
+    languages = data.get('languages', '')
+    region = data.get('region', '')
+    job = data.get('job', '')
+    create_author(name, age, gender, first_language, languages, region, job)
     return jsonify({'success': True})
 
