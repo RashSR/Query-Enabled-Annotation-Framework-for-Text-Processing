473b819aa0c60c4bc16db0253ca9d15691dc1742
rschlager <Reinhold.Schlager@vector.com>
2025-09-08 14:21:02 +0200

    removed annotation checkbox for normal search.

--- mainFlask/static/js/search.js
+++ mainFlask/static/js/search.js
@@ -1,11 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const checkbox = document.getElementById("toggle-annotation");
-    const highlightedElems = document.querySelectorAll(".message-highlighted");
-    const annotatedElems = document.querySelectorAll(".message-annotated");
-
-    checkbox.addEventListener("change", function () {
-        const showAnnotated = checkbox.checked;
-        highlightedElems.forEach(el => el.style.display = showAnnotated ? "none" : "block");
-        annotatedElems.forEach(el => el.style.display = showAnnotated ? "block" : "none");
-    });
-});
+// ...existing code...
