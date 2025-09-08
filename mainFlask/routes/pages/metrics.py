c111a51834156b8b45eff700d293492270e9dc44
rschlager <Reinhold.Schlager@vector.com>
2025-09-08 13:56:52 +0200

    Started with metrics view.

--- mainFlask/routes/pages/metrics.py
+++ mainFlask/routes/pages/metrics.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from mainFlask.data.cachestore import CacheStore
 
 metrics_bp = Blueprint('metrics', __name__)
 
 @metrics_bp.route("/metrics")
 def metrics_view():
-    return render_template('metrics.html')
+    authors = CacheStore.Instance().get_all_authors()
+    return render_template('metrics.html', authors=authors)
