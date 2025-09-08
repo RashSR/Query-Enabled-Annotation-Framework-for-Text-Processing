3d32da4c2e0455ed579d492d5b20e067fc79df23
rschlager <Reinhold.Schlager@vector.com>
2025-09-08 14:00:45 +0200

    added correct jump to metrics view.

--- mainFlask/routes/pages/metrics.py
+++ mainFlask/routes/pages/metrics.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from mainFlask.data.cachestore import CacheStore
 
 metrics_bp = Blueprint('metrics', __name__)
@@ -7,3 +7,10 @@ metrics_bp = Blueprint('metrics', __name__)
 def metrics_view():
     authors = CacheStore.Instance().get_all_authors()
     return render_template('metrics.html', authors=authors)
+
+
+@metrics_bp.route("/metrics/<int:author_id>")
+def metrics_author(author_id):
+    authors = CacheStore.Instance().get_all_authors()
+    # You can add more logic here to show metrics for the selected author
+    return render_template('metrics.html', authors=authors, selected_author_id=author_id)
