dc6615c3de8d5277536fec9239af7fb80ebf9421
rschlager <Reinhold.Schlager@vector.com>
2025-09-09 15:43:26 +0200

    fixed crashing bug in metrics view for no selected author.

--- mainFlask/routes/pages/metrics.py
+++ mainFlask/routes/pages/metrics.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, render_template, request, abort
+from flask import Blueprint, render_template, request, abort, session
 from mainFlask.data.cachestore import CacheStore
+import utils
 
 metrics_bp = Blueprint('metrics', __name__)
 
 @metrics_bp.route("/metrics")
 def metrics_view():
     all_authors = CacheStore.Instance().get_all_authors()
-    return render_template('metrics.html', authors=all_authors)
+    return render_template('metrics.html', authors=all_authors, author=utils.get_active_author(session))
 
 
 @metrics_bp.route("/metrics/<int:author_id>")
